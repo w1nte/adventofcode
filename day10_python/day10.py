c15750e240c578a2c2b2091bfa64c695ac9d75e4
w1nte <github@winterspacher.at>
2020-12-10 09:56:09 +0100

    day10b

--- day10_python/day10.py
+++ day10_python/day10.py
@@ -1,11 +1,15 @@
 import sys
 
-input = sorted(list(map(lambda x: int(x), sys.stdin)))
+input = list(map(lambda x: int(x), sys.stdin))
+input.append(0)
+input = sorted(input)
 charging_outlet = 0
 device_joltage = input[-1] + 3
 input.append(device_joltage)
 
 diff_map = {1: 0, 2: 0, 3: 0}
+groups = []
+group_count = 0
 current_joltage = charging_outlet
 for joltage in input:
     difference = joltage - current_joltage
@@ -13,4 +17,22 @@ for joltage in input:
         diff_map[difference] += 1
         current_joltage = joltage
 
+    if difference == 3:
+        groups.append(group_count)
+        group_count = 0
+
+    group_count += 1
+
+groups.append(group_count)
+
+t = 1
+for g in groups:
+    if g == 3:
+        t *= 2
+    if g == 4:
+        t *= 4
+    if g == 5:
+        t *= 7
+
 print(diff_map[1] * diff_map[3])
+print(t)
