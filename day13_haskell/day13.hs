fd1c86680c44c61fb968420ac88e843ad06c8510
w1nte <github@winterspacher.at>
2020-12-13 22:12:28 +0100

    day13b revisited with pure math

--- day13_haskell/day13.hs
+++ day13_haskell/day13.hs
@@ -1,26 +1,20 @@
 import qualified Data.Text as T
 
-type Bus = Int
-type Time = Int
-
 parseIDs :: String -> [Int]
 parseIDs s = map (read . T.unpack . (\x -> if x == T.pack "x" then T.pack "1" else x)) (T.splitOn (T.pack ",") (T.pack s))
 
-isBus :: Bus -> Time -> Bool
-isBus bus time = mod time bus == 0
+gcdext :: Int -> Int -> (Int, Int)
+gcdext a 0 = (1, 0)
+gcdext a b = (t, s - q * t) where
+    (q, r) = quotRem a b
+    (s, t) = gcdext b r
 
-findFirst :: Bus -> Bus -> Time -> Int -> Int -> Int -> Int
-findFirst b1 b2 time step r1 r2 = if isBus b1 (time + r1) && isBus b2 (time + r2) then time else findFirst b1 b2 (time + step) step r1 r2
-
-stepSize :: Bus -> Bus -> Int
-stepSize = lcm
-
-findSolution :: [Bus] -> Time -> Int -> Int -> Int
-findSolution [_] time _ _ = time
-findSolution (b1:bs) time step r = findSolution bs nextTime nextStep (r+1) where
-    b2 = head bs
-    nextStep = stepSize step b2
-    nextTime = findFirst b1 b2 time step r (r+1)
+solve :: [Int] -> Int -> Int -> Int
+solve [] _ _ = 0
+solve (b:bs) i m = e * (b-i) + solve bs (i+1) m where
+    (r, s) = gcdext b mb
+    e = mb * s
+    mb = m `div` b
 
 main :: IO ()
 main = do  
@@ -28,5 +22,6 @@ main = do
     let input = lines raw
     let ids = parseIDs (input !! 1)
 
-    print ids
-    print (findSolution ids 0 (head ids) 0)
+    let m = foldr1 lcm ids
+    let s = solve ids 0 m
+    print (s - m * (s `div` m))
