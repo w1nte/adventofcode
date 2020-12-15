c7de4f9ae2d1c8442475bfe487352c75f384445d
w1nte <github@winterspacher.at>
2020-12-15 10:42:32 +0100

    day15a

---
+++ day15_python/day15.py
@@ -0,0 +1,23 @@
+import sys
+
+input = []
+for line in sys.stdin:
+    input = line.split(',')
+
+
+def play(numbers, turns):
+    nums = {int(i): x+1 for x, i in enumerate(input)}
+    start = 0
+    n = start
+    for t in range(len(numbers)+1, turns):
+        nsp = n
+        if nsp not in nums:
+            nums[nsp] = t
+            n = start
+            continue
+        n = t - nums[n]
+        nums[nsp] = t
+    return n
+
+print(play(input, 2020))
+
