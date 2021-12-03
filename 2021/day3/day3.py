409ef4ecd86c2be072a2f2179378aa8ebad22e15
w1nte <github@winterspacher.at>
2021-12-03 14:13:53 +0100

    day3b

--- 2021/day3/day3.py
+++ 2021/day3/day3.py
@@ -1,8 +1,32 @@
 import sys
 from statistics import mode
 
-b = [sys.stdin.read().splitlines()][0]
-g = int(''.join([mode([x[i] for x in b]) for i in range(0, len(b[0]))]), 2)
-e = int(''.join(['0' if mode([x[i] for x in b]) == '1' else '1' for i in range(0, len(b[0]))]), 2)
+def c(l):
+    a,b = (0,0)
+    for x in l:
+        if x == '1':
+            a+=1
+        if x == '0':
+            b+=1
+    return (a,b)
 
-print(g*e)
+b = [sys.stdin.read().splitlines()][0]
+g = [c([x[i] for x in b]) for i in range(0, len(b[0]))]
+
+r, p = (b, 0)
+while len(r) > 1:
+    r = [x for x in r if x[p] == '1' and g[p][0] > g[p][1] or x[p] == '0' and g[p][0] < g[p][1] or x[p] == '1' and g[p][0] == g[p][1]]
+    g = [c([x[i] for x in r]) for i in range(0, len(b[0]))]
+    p += 1
+    
+a1 = int(''.join(r), 2)
+
+r, p = (b, 0)
+while len(r) > 1:
+    r = [x for x in r if x[p] == '1' and g[p][0] < g[p][1] or x[p] == '0' and g[p][0] > g[p][1] or x[p] == '0' and g[p][0] == g[p][1]]
+    g = [c([x[i] for x in r]) for i in range(0, len(b[0]))]
+    p += 1
+
+b1 = int(''.join(r), 2)
+
+print(a1 * b1)
