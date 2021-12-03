2b27d336fd0c604ff2e66061af2f86df136884a0
w1nte <github@winterspacher.at>
2021-12-03 08:59:04 +0100

    day1b

--- 2021/day1/day1.py
+++ 2021/day1/day1.py
@@ -1,7 +1,9 @@
-import sys, functools
+import sys, re
 
+l = [int(x) for x in sys.stdin]
+nl = [l[i] + l[i-1] + l[i+1] for i in range(1, len(l)-1)]
 c, o = -1, 0
-for y in [int(x) for x in sys.stdin]:
+for y in nl:
     if y > o:
         c += 1
     o = y
